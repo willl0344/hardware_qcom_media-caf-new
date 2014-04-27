CyanogenMod/android_hardware_qcom_media

mm-video: venc: Add support for WFD controls
Add QC extension to configure performance level,
VUI timing info, peak bitrate.

Change-Id: I854d09d8b4d42b19fdb5867baa08662bf432cc59

mm-core/inc/OMX_QCOMExtns.h
@@ -446,6 +446,12 @@ enum OMX_QCOM_EXTN_INDEXTYPE
 
     /* VP8 Hierarchical P support */
     OMX_QcomIndexHierarchicalStructure = 0x7F000037,
+
+    OMX_QcomIndexParamPerfLevel = 0x7F000038,
+
+    OMX_QcomIndexParamH264VUITimingInfo = 0x7F000039,
+
+    OMX_QcomIndexParamPeakBitrate = 0x7F00003A,
 };
 
 /**
@@ -756,6 +762,45 @@ typedef struct OMX_QCOM_VIDEO_CONFIG_H264_AUD
    OMX_VERSIONTYPE nVersion;/** OMX specification version information */
    OMX_BOOL bEnable;        /** Enable/disable the setting */
 } OMX_QCOM_VIDEO_CONFIG_H264_AUD;
+
+typedef enum QOMX_VIDEO_PERF_LEVEL
+{
+    OMX_QCOM_PerfLevelNominal,
+    OMX_QCOM_PerfLevelTurbo
+} QOMX_VIDEO_PERF_LEVEL;
+
+/**
+ * This structure describes the parameters corresponding
+ * to OMX_QcomIndexParamPerfLevel extension. It will set
+ * the performance mode specified as QOMX_VIDEO_PERF_LEVEL.
+ */
+typedef struct OMX_QCOM_VIDEO_PARAM_PERF_LEVEL {
+    OMX_U32 nSize;                      /** Size of the structure in bytes */
+    OMX_VERSIONTYPE nVersion;           /** OMX specification version information */
+    QOMX_VIDEO_PERF_LEVEL ePerfLevel;   /** Performance level */
+} OMX_QCOM_VIDEO_PARAM_PERF_LEVEL;
+
+/**
+ * This structure describes the parameters corresponding
+ * to OMX_QcomIndexParamH264VUITimingInfo extension. It
+ * will enable/disable the VUI timing info.
+ */
+typedef struct OMX_QCOM_VIDEO_PARAM_VUI_TIMING_INFO {
+    OMX_U32 nSize;              /** Size of the structure in bytes */
+    OMX_VERSIONTYPE nVersion;   /** OMX specification version information */
+    OMX_BOOL bEnable;           /** Enable/disable the setting */
+} OMX_QCOM_VIDEO_PARAM_VUI_TIMING_INFO;
+
+/**
+ * This structure describes the parameters corresponding
+ * to OMX_QcomIndexParamPeakBitrate extension. It will
+ * set the peak bitrate specified by nPeakBitrate.
+ */
+typedef struct OMX_QCOM_VIDEO_PARAM_PEAK_BITRATE {
+    OMX_U32 nSize;              /** Size of the structure in bytes */
+    OMX_VERSIONTYPE nVersion;   /** OMX specification version information */
+    OMX_U32 nPeakBitrate;       /** Peak bitrate value */
+} OMX_QCOM_VIDEO_PARAM_PEAK_BITRATE;
 
 typedef struct OMX_VENDOR_EXTRADATATYPE  {
     OMX_U32 nPortIndex;

mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -1584,6 +1584,45 @@ bool venc_dev::venc_set_param(void *paramData, OMX_INDEXTYPE index)
 
                 break;
            }
+        case OMX_QcomIndexParamPerfLevel:
+            {
+                OMX_QCOM_VIDEO_PARAM_PERF_LEVEL *pParam =
+                        (OMX_QCOM_VIDEO_PARAM_PERF_LEVEL *)paramData;
+                DEBUG_PRINT_LOW("Set perf level: %d", pParam->ePerfLevel);
+                if(!venc_set_perf_level(pParam->ePerfLevel)) {
+                    DEBUG_PRINT_ERROR("ERROR: Failed to set perf level to %d", pParam->ePerfLevel);
+                    return false;
+                } else {
+                    performance_level.perflevel = (unsigned int) pParam->ePerfLevel;
+                }
+                break;
+            }
+        case OMX_QcomIndexParamH264VUITimingInfo:
+            {
+                OMX_QCOM_VIDEO_PARAM_VUI_TIMING_INFO *pParam =
+                        (OMX_QCOM_VIDEO_PARAM_VUI_TIMING_INFO *)paramData;
+                DEBUG_PRINT_LOW("Set VUI timing info: %d", pParam->bEnable);
+                if(venc_set_vui_timing_info(pParam->bEnable) == false) {
+                    DEBUG_PRINT_ERROR("ERROR: Failed to set vui timing info to %d", pParam->bEnable);
+                    return false;
+                } else {
+                    vui_timing_info.enabled = (unsigned int) pParam->bEnable;
+                }
+                break;
+            }
+        case OMX_QcomIndexParamPeakBitrate:
+            {
+                OMX_QCOM_VIDEO_PARAM_PEAK_BITRATE *pParam =
+                        (OMX_QCOM_VIDEO_PARAM_PEAK_BITRATE *)paramData;
+                DEBUG_PRINT_LOW("Set peak bitrate: %lu", pParam->nPeakBitrate);
+                if(venc_set_peak_bitrate(pParam->nPeakBitrate) == false) {
+                    DEBUG_PRINT_ERROR("ERROR: Failed to set peak bitrate to %lu", pParam->nPeakBitrate);
+                    return false;
+                } else {
+                    peak_bitrate.peakbitrate = (unsigned int) pParam->nPeakBitrate;
+                }
+                break;
+            }
         case OMX_IndexParamVideoSliceFMO:
         default:
             DEBUG_PRINT_ERROR("ERROR: Unsupported parameter in venc_set_param: %u",
@@ -1953,6 +1992,11 @@ void venc_dev::venc_config_print()
 
     DEBUG_PRINT_HIGH("ENC_CONFIG: Hier-P layers: %d", hier_p_layers.numlayers);
 
+    DEBUG_PRINT_HIGH("ENC_CONFIG: Performace level: %d", performance_level.perflevel);
+
+    DEBUG_PRINT_HIGH("ENC_CONFIG: VUI timing info enabled: %d", vui_timing_info.enabled);
+
+    DEBUG_PRINT_HIGH("ENC_CONFIG: Peak bitrate: %d", peak_bitrate.peakbitrate);
 }
 
 bool venc_dev::venc_reconfig_reqbufs()
@@ -2147,6 +2191,39 @@ bool venc_dev::venc_color_align(OMX_BUFFERHEADERTYPE *buffer,
     }
 
     return true;
+}
+
+bool venc_dev::venc_get_performance_level(OMX_U32 *perflevel)
+{
+    if (!perflevel) {
+        DEBUG_PRINT_ERROR("Null pointer error");
+        return false;
+    } else {
+        *perflevel = performance_level.perflevel;
+        return true;
+    }
+}
+
+bool venc_dev::venc_get_vui_timing_info(OMX_U32 *enabled)
+{
+    if (!enabled) {
+        DEBUG_PRINT_ERROR("Null pointer error");
+        return false;
+    } else {
+        *enabled = vui_timing_info.enabled;
+        return true;
+    }
+}
+
+bool venc_dev::venc_get_peak_bitrate(OMX_U32 *peakbitrate)
+{
+    if (!peakbitrate) {
+        DEBUG_PRINT_ERROR("Null pointer error");
+        return false;
+    } else {
+        *peakbitrate = peak_bitrate.peakbitrate;
+        return true;
+    }
 }
 
 bool venc_dev::venc_empty_buf(void *buffer, void *pmem_data_buf, unsigned index, unsigned fd)
@@ -3605,6 +3682,82 @@ bool venc_dev::venc_set_ratectrl_cfg(OMX_VIDEO_CONTROLRATETYPE eControlRate)
     }
 
     return status;
+}
+
+bool venc_dev::venc_set_perf_level(QOMX_VIDEO_PERF_LEVEL ePerfLevel)
+{
+    bool status = true;
+    struct v4l2_control control;
+    int rc = 0;
+    control.id = V4L2_CID_MPEG_VIDC_SET_PERF_LEVEL;
+
+    switch (ePerfLevel) {
+    case OMX_QCOM_PerfLevelNominal:
+        control.value = V4L2_CID_MPEG_VIDC_PERF_LEVEL_NOMINAL;
+        break;
+    case OMX_QCOM_PerfLevelTurbo:
+        control.value = V4L2_CID_MPEG_VIDC_PERF_LEVEL_TURBO;
+        break;
+    default:
+        status = false;
+        break;
+    }
+
+    if (status) {
+        DEBUG_PRINT_LOW("Calling IOCTL set control for id=%d, val=%d", control.id, control.value);
+        rc = ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control);
+
+        if (rc) {
+            DEBUG_PRINT_ERROR("Failed to set control for id=%d, val=%d", control.id, control.value);
+            return false;
+        }
+
+        DEBUG_PRINT_LOW("Success IOCTL set control for id=%d, value=%d", control.id, control.value);
+    }
+    return status;
+}
+
+bool venc_dev::venc_set_vui_timing_info(OMX_BOOL enable)
+{
+    struct v4l2_control control;
+    int rc = 0;
+    control.id = V4L2_CID_MPEG_VIDC_VIDEO_H264_VUI_TIMING_INFO;
+
+    if (enable)
+        control.value = V4L2_MPEG_VIDC_VIDEO_H264_VUI_TIMING_INFO_ENABLED;
+    else
+        control.value = V4L2_MPEG_VIDC_VIDEO_H264_VUI_TIMING_INFO_DISABLED;
+
+    DEBUG_PRINT_LOW("Calling IOCTL set control for id=%x, val=%d", control.id, control.value);
+    rc = ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control);
+    if (rc) {
+        DEBUG_PRINT_ERROR("Failed to set VUI timing info control");
+        return false;
+    }
+    DEBUG_PRINT_LOW("Success IOCTL set control for id=%x, value=%d", control.id, control.value);
+    return true;
+}
+
+bool venc_dev::venc_set_peak_bitrate(OMX_U32 nPeakBitrate)
+{
+    struct v4l2_control control;
+    int rc = 0;
+    control.id = V4L2_CID_MPEG_VIDEO_BITRATE_PEAK;
+    control.value = nPeakBitrate;
+
+    DEBUG_PRINT_LOW("venc_set_peak_bitrate: bitrate = %lu", nPeakBitrate);
+
+    DEBUG_PRINT_LOW("Calling IOCTL set control for id=%d, val=%d", control.id, control.value);
+    rc = ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control);
+
+    if (rc) {
+        DEBUG_PRINT_ERROR("Failed to set peak bitrate control");
+        return false;
+    }
+
+    DEBUG_PRINT_LOW("Success IOCTL set control for id=%d, value=%d", control.id, control.value);
+
+    return true;
 }
 
 bool venc_dev::venc_get_profile_level(OMX_U32 *eProfile,OMX_U32 *eLevel)

willl0344/hardware_qcom_media-caf-new

mm-core/inc/OMX_QCOMExtns.h
@@ -439,6 +439,12 @@ enum OMX_QCOM_EXTN_INDEXTYPE
 
     /*"OMX.QCOM.index.config.video.LTRMark"*/
     OMX_QcomIndexConfigVideoLTRMark = QOMX_IndexConfigVideoLTRMark,
+
+    OMX_QcomIndexParamPerfLevel = 0x7F000032,
+
+    OMX_QcomIndexParamH264VUITimingInfo = 0x7F000033,
+
+    OMX_QcomIndexParamPeakBitrate = 0x7F000034,
 };
 
 /**
@@ -768,6 +774,45 @@ typedef struct OMX_QCOM_VIDEO_CONFIG_H264_AUD
    OMX_VERSIONTYPE nVersion;/** OMX specification version information */
    OMX_BOOL bEnable;        /** Enable/disable the setting */
 } OMX_QCOM_VIDEO_CONFIG_H264_AUD;
+
+typedef enum QOMX_VIDEO_PERF_LEVEL
+{
+    OMX_QCOM_PerfLevelNominal,
+    OMX_QCOM_PerfLevelTurbo
+} QOMX_VIDEO_PERF_LEVEL;
+
+/**
+ * This structure describes the parameters corresponding
+ * to OMX_QcomIndexParamPerfLevel extension. It will set
+ * the performance mode specified as QOMX_VIDEO_PERF_LEVEL.
+ */
+typedef struct OMX_QCOM_VIDEO_PARAM_PERF_LEVEL {
+    OMX_U32 nSize;                      /** Size of the structure in bytes */
+    OMX_VERSIONTYPE nVersion;           /** OMX specification version information */
+    QOMX_VIDEO_PERF_LEVEL ePerfLevel;   /** Performance level */
+} OMX_QCOM_VIDEO_PARAM_PERF_LEVEL;
+
+/**
+ * This structure describes the parameters corresponding
+ * to OMX_QcomIndexParamH264VUITimingInfo extension. It
+ * will enable/disable the VUI timing info.
+ */
+typedef struct OMX_QCOM_VIDEO_PARAM_VUI_TIMING_INFO {
+    OMX_U32 nSize;              /** Size of the structure in bytes */
+    OMX_VERSIONTYPE nVersion;   /** OMX specification version information */
+    OMX_BOOL bEnable;           /** Enable/disable the setting */
+} OMX_QCOM_VIDEO_PARAM_VUI_TIMING_INFO;
+
+/**
+ * This structure describes the parameters corresponding
+ * to OMX_QcomIndexParamPeakBitrate extension. It will
+ * set the peak bitrate specified by nPeakBitrate.
+ */
+typedef struct OMX_QCOM_VIDEO_PARAM_PEAK_BITRATE {
+    OMX_U32 nSize;              /** Size of the structure in bytes */
+    OMX_VERSIONTYPE nVersion;   /** OMX specification version information */
+    OMX_U32 nPeakBitrate;       /** Peak bitrate value */
+} OMX_QCOM_VIDEO_PARAM_PEAK_BITRATE;
 
 typedef struct OMX_VENDOR_EXTRADATATYPE  {
     OMX_U32 nPortIndex;

mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -1654,6 +1654,45 @@ bool venc_dev::venc_set_param(void *paramData, OMX_INDEXTYPE index)
                 }
                 break;
             }
+        case OMX_QcomIndexParamPerfLevel:
+            {
+                OMX_QCOM_VIDEO_PARAM_PERF_LEVEL *pParam =
+                        (OMX_QCOM_VIDEO_PARAM_PERF_LEVEL *)paramData;
+                DEBUG_PRINT_LOW("Set perf level: %d\n", pParam->ePerfLevel);
+                if (venc_set_perf_level(pParam->ePerfLevel) == false) {
+                    DEBUG_PRINT_ERROR("ERROR: Failed to set perf level to %d", pParam->ePerfLevel);
+                    return false;
+                } else {
+                    performance_level.perflevel = (unsigned int) pParam->ePerfLevel;
+                }
+                break;
+            }
+        case OMX_QcomIndexParamH264VUITimingInfo:
+            {
+                OMX_QCOM_VIDEO_PARAM_VUI_TIMING_INFO *pParam =
+                        (OMX_QCOM_VIDEO_PARAM_VUI_TIMING_INFO *)paramData;
+                DEBUG_PRINT_LOW("Set VUI timing info: %d\n", pParam->bEnable);
+                if (venc_set_vui_timing_info(pParam->bEnable) == false) {
+                    DEBUG_PRINT_ERROR("ERROR: Failed to set vui timing info to %d", pParam->bEnable);
+                    return false;
+                } else {
+                    vui_timing_info.enabled = (unsigned int) pParam->bEnable;
+                }
+                break;
+            }
+        case OMX_QcomIndexParamPeakBitrate:
+            {
+                OMX_QCOM_VIDEO_PARAM_PEAK_BITRATE *pParam =
+                        (OMX_QCOM_VIDEO_PARAM_PEAK_BITRATE *)paramData;
+                DEBUG_PRINT_LOW("Set peak bitrate: %lu\n", pParam->nPeakBitrate);
+                if (venc_set_peak_bitrate(pParam->nPeakBitrate) == false) {
+                    DEBUG_PRINT_ERROR("ERROR: Failed to set peak bitrate to %lu", pParam->nPeakBitrate);
+                    return false;
+                } else {
+                    peak_bitrate.peakbitrate = (unsigned int) pParam->nPeakBitrate;
+                }
+                break;
+            }
         case OMX_IndexParamVideoSliceFMO:
         default:
             DEBUG_PRINT_ERROR("ERROR: Unsupported parameter in venc_set_param: %u",
@@ -2042,6 +2081,12 @@ void venc_dev::venc_config_print()
 
     DEBUG_PRINT_HIGH("ENC_CONFIG: LTR Enabled: %d, Count: %d",
             ltrinfo.enabled, ltrinfo.count);
+
+    DEBUG_PRINT_HIGH("ENC_CONFIG: Performace level: %d", performance_level.perflevel);
+
+    DEBUG_PRINT_HIGH("ENC_CONFIG: VUI timing info enabled: %d", vui_timing_info.enabled);
+
+    DEBUG_PRINT_HIGH("ENC_CONFIG: Peak bitrate: %d", peak_bitrate.peakbitrate);
 }
 
 bool venc_dev::venc_reconfig_reqbufs()
@@ -2236,6 +2281,39 @@ bool venc_dev::venc_color_align(OMX_BUFFERHEADERTYPE *buffer,
     }
 
     return true;
+}
+
+bool venc_dev::venc_get_performance_level(OMX_U32 *perflevel)
+{
+    if (!perflevel) {
+        DEBUG_PRINT_ERROR("Null pointer error");
+        return false;
+    } else {
+        *perflevel = performance_level.perflevel;
+        return true;
+    }
+}
+
+bool venc_dev::venc_get_vui_timing_info(OMX_U32 *enabled)
+{
+    if (!enabled) {
+        DEBUG_PRINT_ERROR("Null pointer error");
+        return false;
+    } else {
+        *enabled = vui_timing_info.enabled;
+        return true;
+    }
+}
+
+bool venc_dev::venc_get_peak_bitrate(OMX_U32 *peakbitrate)
+{
+    if (!peakbitrate) {
+        DEBUG_PRINT_ERROR("Null pointer error");
+        return false;
+    } else {
+        *peakbitrate = peak_bitrate.peakbitrate;
+        return true;
+    }
 }
 
 bool venc_dev::venc_empty_buf(void *buffer, void *pmem_data_buf, unsigned index, unsigned fd)
@@ -3691,6 +3769,82 @@ bool venc_dev::venc_set_ratectrl_cfg(OMX_VIDEO_CONTROLRATETYPE eControlRate)
     }
 
     return status;
+}
+
+bool venc_dev::venc_set_perf_level(QOMX_VIDEO_PERF_LEVEL ePerfLevel)
+{
+    bool status = true;
+    struct v4l2_control control;
+    int rc = 0;
+    control.id = V4L2_CID_MPEG_VIDC_SET_PERF_LEVEL;
+
+    switch (ePerfLevel) {
+    case OMX_QCOM_PerfLevelNominal:
+        control.value = V4L2_CID_MPEG_VIDC_PERF_LEVEL_NOMINAL;
+        break;
+    case OMX_QCOM_PerfLevelTurbo:
+        control.value = V4L2_CID_MPEG_VIDC_PERF_LEVEL_TURBO;
+        break;
+    default:
+        status = false;
+        break;
+    }
+
+    if (status) {
+        DEBUG_PRINT_LOW("Calling IOCTL set control for id=%d, val=%d", control.id, control.value);
+        rc = ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control);
+
+        if (rc) {
+            DEBUG_PRINT_ERROR("Failed to set control for id=%d, val=%d", control.id, control.value);
+            return false;
+        }
+
+        DEBUG_PRINT_LOW("Success IOCTL set control for id=%d, value=%d", control.id, control.value);
+    }
+    return status;
+}
+
+bool venc_dev::venc_set_vui_timing_info(OMX_BOOL enable)
+{
+    struct v4l2_control control;
+    int rc = 0;
+    control.id = V4L2_CID_MPEG_VIDC_VIDEO_H264_VUI_TIMING_INFO;
+
+    if (enable)
+        control.value = V4L2_MPEG_VIDC_VIDEO_H264_VUI_TIMING_INFO_ENABLED;
+    else
+        control.value = V4L2_MPEG_VIDC_VIDEO_H264_VUI_TIMING_INFO_DISABLED;
+
+    DEBUG_PRINT_LOW("Calling IOCTL set control for id=%x, val=%d", control.id, control.value);
+    rc = ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control);
+    if (rc) {
+        DEBUG_PRINT_ERROR("Failed to set VUI timing info control");
+        return false;
+    }
+    DEBUG_PRINT_LOW("Success IOCTL set control for id=%x, value=%d", control.id, control.value);
+    return true;
+}
+
+bool venc_dev::venc_set_peak_bitrate(OMX_U32 nPeakBitrate)
+{
+    struct v4l2_control control;
+    int rc = 0;
+    control.id = V4L2_CID_MPEG_VIDEO_BITRATE_PEAK;
+    control.value = nPeakBitrate;
+
+    DEBUG_PRINT_LOW("venc_set_peak_bitrate: bitrate = %lu", nPeakBitrate);
+
+    DEBUG_PRINT_LOW("Calling IOCTL set control for id=%d, val=%d", control.id, control.value);
+    rc = ioctl(m_nDriver_fd, VIDIOC_S_CTRL, &control);
+
+    if (rc) {
+        DEBUG_PRINT_ERROR("Failed to set peak bitrate control");
+        return false;
+    }
+
+    DEBUG_PRINT_LOW("Success IOCTL set control for id=%d, value=%d", control.id, control.value);
+
+    return true;
 }
 
 bool venc_dev::venc_get_profile_level(OMX_U32 *eProfile,OMX_U32 *eLevel)