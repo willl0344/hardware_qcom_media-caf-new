CyanogenMod/android_hardware_qcom_media

dashplayer: Enable GraphicBuffer usage to decoder before querying formats
This lets the decoder publish appropriate color format for surface-mode
playback. (For non-surface mode, a standard YUV format will be published)

Change-Id: Ic80d00aef027c84beddc5e23c2cfc3846dbbad43

dashplayer/DashCodec.cpp
@@ -3558,19 +3558,6 @@ bool DashCodec::LoadedState::onConfigureComponent(
         mCodec->mFlags |= kFlagIsSecureOPOnly;
     }
 
-    AString mime;
-    CHECK(msg->findString("mime", &mime));
-
-    status_t err = mCodec->configureCodec(mime.c_str(), msg);
-
-    if (err != OK) {
-        DC_MSG_ERROR("[%s] configureCodec returning error %d",
-              mCodec->mComponentName.c_str(), err);
-
-        mCodec->signalError(OMX_ErrorUndefined, err);
-        return false;
-    }
-
     sp<RefBase> obj;
     if (msg->findObject("native-window", &obj)
             && strncmp("OMX.google.", mCodec->mComponentName.c_str(), 11)) {
@@ -3584,6 +3571,19 @@ bool DashCodec::LoadedState::onConfigureComponent(
                 NATIVE_WINDOW_SCALING_MODE_SCALE_TO_WINDOW);
     }
     CHECK_EQ((status_t)OK, mCodec->initNativeWindow());
+
+    AString mime;
+    CHECK(msg->findString("mime", &mime));
+
+    status_t err = mCodec->configureCodec(mime.c_str(), msg);
+
+    if (err != OK) {
+        DC_MSG_ERROR("[%s] configureCodec returning error %d",
+              mCodec->mComponentName.c_str(), err);
+
+        mCodec->signalError(OMX_ErrorUndefined, err);
+        return false;
+    }
 
     {
         sp<AMessage> notify = mCodec->mNotify->dup();

willl0344/hardware_qcom_media-caf-new

dashplayer/DashCodec.cpp
@@ -3571,19 +3571,6 @@ bool DashCodec::LoadedState::onConfigureComponent(
         mCodec->mFlags |= kFlagIsSecureOPOnly;
     }
 
-    AString mime;
-    CHECK(msg->findString("mime", &mime));
-
-    status_t err = mCodec->configureCodec(mime.c_str(), msg);
-
-    if (err != OK) {
-        ALOGE("[%s] configureCodec returning error %d",
-              mCodec->mComponentName.c_str(), err);
-
-        mCodec->signalError(OMX_ErrorUndefined, err);
-        return false;
-    }
-
     sp<RefBase> obj;
     if (msg->findObject("native-window", &obj)
             && strncmp("OMX.google.", mCodec->mComponentName.c_str(), 11)) {
@@ -3597,6 +3584,19 @@ bool DashCodec::LoadedState::onConfigureComponent(
                 NATIVE_WINDOW_SCALING_MODE_SCALE_TO_WINDOW);
     }
     CHECK_EQ((status_t)OK, mCodec->initNativeWindow());
+
+    AString mime;
+    CHECK(msg->findString("mime", &mime));
+
+    status_t err = mCodec->configureCodec(mime.c_str(), msg);
+
+    if (err != OK) {
+        ALOGE("[%s] configureCodec returning error %d",
+              mCodec->mComponentName.c_str(), err);
+
+        mCodec->signalError(OMX_ErrorUndefined, err);
+        return false;
+    }
 
     {
         sp<AMessage> notify = mCodec->mNotify->dup();